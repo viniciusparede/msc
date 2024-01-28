cac8924b27c5216078ffea35c0b4b44a73608073
Vinicius Parede <vinicius.parede1@gmail.com>
2024-01-28 15:23:24 -0300

    make a dataset

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from mmsdk import mmdatasdk as md
 
 # caminho para o arquivo .csd
-csd_path = "/home/vinicius/Documentos/Repositories/test-cmumosei/data/CMU_MOSEI_TimestampedWordVectors.csd"
+csd_path = "/home/vinicius/Documentos/Repositories/msc/data"
 
 # carregar o arquivo .csd como um novo conjunto de dados
 dataset = md.mmdataset(csd_path)
+
+dataset.align("Opinion Segment Labels")
 
 # agora você pode trabalhar com 'dataset' conforme necessário
